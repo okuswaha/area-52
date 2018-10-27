215dac322593830230f99f5fe653caaf0bf8f7fc
om <kuswaha.java@gmail.com>
2018-10-27 18:18:44 -0400

    adding new transactions

--- perishable-network/lib/logic.js
+++ perishable-network/lib/logic.js
@@ -9,3 +9,77 @@ async function setupDemo(setupDemo) {  // eslint-disable-line no-unused-vars
     const NS = 'com.routeone.area52';
 
 }
+
+/**
+ * Initialize some test assets and participants useful for running a demo.
+ * @param {com.routeone.area52.SetupDemo} setupDemo - the SetupDemo transaction
+ * @transaction
+ */
+async function setupDemo(setupDemo) {  // eslint-disable-line no-unused-vars
+
+    const factory = getFactory();
+    const NS = 'com.routeone.area52';
+    console.log('SetUpDemo transacion occured');
+  }
+  
+  /**
+   * Initialize some test assets and participants useful for running a demo.
+   * @param {com.routeone.area52.AddCarToRegisty} addCarToRegisty - the AddCarToRegisty transaction
+   * @transaction
+   */
+  async function addCarToRegisty(addCarToRegisty) {  // eslint-disable-line no-unused-vars
+  
+    const factory = getFactory();
+    const NS = 'com.routeone.area52';
+    console.log('A Car was added to Registry');
+  }
+  
+  /**
+   * Initialize some test assets and participants useful for running a demo.
+   * @param {com.routeone.area52.CustomerCreditApproved} customerCreditApproved - the AddCarToRegisty transaction
+   * @transaction
+   */
+  async function customerCreditApproved(customerCreditApproved) {  // eslint-disable-line no-unused-vars
+  
+    const factory = getFactory();
+    const NS = 'com.routeone.area52';
+    console.log('A Customer is approved for loan');
+  }
+  
+  /**
+   * Initialize some test assets and participants useful for running a demo.
+   * @param {com.routeone.area52.PaySubscriptionFee} paySubscriptionFee - the PaySubscriptionFee transaction
+   * @transaction
+   */
+  async function PaySubscriptionFee(paySubscriptionFee) {  // eslint-disable-line no-unused-vars
+  
+    const factory = getFactory();
+    const NS = 'com.routeone.area52';
+    console.log('customer pays a subscription fee');
+  }
+  
+  /**
+   * Initialize some test assets and participants useful for running a demo.
+   * @param {com.routeone.area52.RentCar} rentCar - the RentCar transaction
+   * @transaction
+   */
+  async function rentCar(rentCar) {  // eslint-disable-line no-unused-vars
+  
+    const factory = getFactory();
+    const NS = 'com.routeone.area52';
+    console.log('customer rents a car');
+  }
+  
+  /**
+   * Initialize some test assets and participants useful for running a demo.
+   * @param {com.routeone.area52.ReturnCar} ReturnCar - the RentCar transaction
+   * @transaction
+   */
+  async function returnCar(returnCar) {  // eslint-disable-line no-unused-vars
+  
+    const factory = getFactory();
+    const NS = 'com.routeone.area52';
+    console.log('customer returns a car');
+  }
+  
+  
